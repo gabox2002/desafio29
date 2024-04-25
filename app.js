60973ad0f289b0ea4bebcbbc456ac3c17ee8ed38
gabox2002 <gabox2002@gmail.com>
2024-04-24 23:54:41 -0300

    m

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const fs = require("fs");
 const util = require("util");
 
 const readFile = () => {
-    const file = fs.readFileSync("./desafio29.txt");
-    const data = file.toString();
-    const processInfo = util.inspect(process);
-    const rows = data.split(/\r?\n/);
+    const file = fs.readFileSync("./desafio29.txt")
+    const data = file.toString()
+    const processInfo = util.inspect(process)
+    const rows = data.split(/\r?\n/)
     const matriz = rows
         .filter((row) => row.trim() !== "")
-        .map((row) => row.split(" "));
-    console.log(matriz);
-    console.log(`Información del proceso: ${processInfo}`);
+        .map((row) => row.split(" "))
+    //console.log(matriz);
+    console.log(`Información del proceso: ${processInfo}`)
 
     const lineCount = matriz.length;
-    console.log(`Números de líneas: ${lineCount}`);
+    console.log(`Números de líneas: ${lineCount}`)
 
-    const totalWords = matriz.reduce((acc, row) => acc + row.length, 0);
-    console.log(`Número de palabras: ${totalWords}`);
+    const totalWords = matriz.reduce((acc, row) => acc + row.length, 0)
+    console.log(`Número de palabras: ${totalWords}`)
 
     const charCount = data.replace(/\s/g, "").length;
-    console.log(`Número de caracteres: ${charCount}`);
-};
+    console.log(`Número de caracteres: ${charCount}`)
+}
 
-readFile();
+readFile()
